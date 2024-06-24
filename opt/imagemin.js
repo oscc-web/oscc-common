8f1e94ffbddb10660edd8b82c8e4dac0ca9b6396
myyerrol <myyerrol@126.com>
2024-06-24 23:47:52 +0800

    feat: add imagemin gif tools

--- opt/imagemin.js
+++ opt/imagemin.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from "url";
 import imagemin from "imagemin";
 import imageminJpegtran from "imagemin-jpegtran";
 import imageminPngquant from "imagemin-pngquant";
+import imagemingiflossy from "imagemin-giflossy";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -31,7 +32,7 @@ function readDirtory(dir, newDir) {
             readDirtory(totalDir, childDir)
         }
         else {
-            if ([".png", ".jpg", ".jpeg"].indexOf(path.extname(totalDir)) !== -1) {
+            if ([".png", ".jpg", ".jpeg", ".gif"].indexOf(path.extname(totalDir)) !== -1) {
                 parseFiles(totalDir,childDir)
             }
         }
@@ -45,9 +46,72 @@ function parseFiles(fileSrcs, fileDest) {
             destination: path.dirname(fileDest),
             glob: false,
             plugins: [
-                imageminJpegtran(),
+                // https://www.npmjs.com/package/imagemin-jpegtran
+                imageminJpegtran({
+                    // Values: true or false
+                    // Lossless conversion to progressive.
+                    progressive: false,
+                    // Values: true or false
+                    // Arithmetic coding (https://en.wikipedia.org/wiki/Arithmetic_coding)
+                    arithmetic: false
+                }),
+                // https://www.npmjs.com/package/imagemin-pngquant
                 imageminPngquant({
-                    quality: [0.6, 0.8]
+                    // Values: 1 (brute-force) to 11 (fastest).
+                    // Speed 10 has 5% lower quality, but is about 8 times
+                    // faster than the default. Speed 11 disables dithering and
+                    // lowers compression level.
+                    speed: 4,
+                    // Values: true or false
+                    // Remove optional metadata.
+                    strip: false,
+                    // Values: [0-1, 0-1]
+                    // Instructs pngquant to use the least amount of colors
+                    // required to meet or exceed the max quality. If
+                    // conversion results in quality below the min quality the
+                    // image won't be saved.
+                    quality: [0.6, 0.8],
+                    // Values: 0-1
+                    // Set the dithering level using a fractional number
+                    // between 0 (none) and 1 (full).
+                    dithering: 1
+                }),
+                // https://www.npmjs.com/package/imagemin-giflossy
+                imagemingiflossy({
+                    // Values: true or false
+                    // Interlace gif for progressive rendering.
+                    interlaced: false,
+                    // Values: 1 or 2 or 3
+                    // Select an optimization level between 1 and 3.
+                    // The optimization level determines how much optimization
+                    // is done; higher levels take longer, but may have better
+                    // results.
+                    optimizationLevel: 1,
+                    // Values: 80-200
+                    // Order pixel patterns to create smaller GIFs at cost of
+                    // artifacts and noise.
+                    // Adjust lossy argument to quality you want (30 is very
+                    // light compression, 200 is heavy).
+                    lossy: 80,
+                    // Values: undefined or "300x200" or etc
+                    // Resize the output GIF to widthxheight
+                    resize: undefined,
+                    // Values: true or false
+                    // Sets the output logical screen to the size of the
+                    // largest output frame.
+                    noLogicalScreen: false,
+                    // Values: "mix" or "sample" or etc
+                    // Set the method used to resize images.
+                    resizeMethod: "mix",
+                    // Values: "diversity" or "blend-diversity" or etc
+                    // Determine how a smaller colormap is chosen
+                    colorMethod: "diversity",
+                    // Values: 1 or 2 or 3
+                    // Optimize output GIF animations for space.
+                    optimize: 1,
+                    // Values: true or false
+                    // Unoptimize GIF animations into an easy-to-edit form.
+                    unoptimize: false
                 })
             ]
         })
